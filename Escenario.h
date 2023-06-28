8c30996622526de58c47261e3750af7b51e970f6
juanjosevel <104180578+juanjosevel@users.noreply.github.com>
2023-06-28 11:36:06 -0500

    Update Escenario.h

--- Escenario.h
+++ Escenario.h
@@ -29,7 +29,7 @@ namespace juego {
             bool update();
             Position getPosition() const { return Position(); }
             shared_ptr<Jugador> getPS() const { return ps_; }
-            shared_ptr<EntityList> get_enemyships() const { return tipoCarta; }
+            shared_ptr<EntityList> get_tipoCarta() const { return tipoCarta; }
             shared_ptr<EntityList> get_cartas() const { return cartas; }
         };
 
